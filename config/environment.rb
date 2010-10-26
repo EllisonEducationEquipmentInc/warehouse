30d49ea9fe3f25a9c0fddd7cf41559067181485b
jdacio <jdacio@4d8cb25c-e031-4999-ae5c-9f206818a7d3>
2010-10-26 20:52:22 +0000

    add sample csv

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # set to false if you want to use the app in 'warehouse' mode
-TRADESHOW = true 
-#TRADESHOW = false
+#TRADESHOW = true 
+TRADESHOW = false
 
 # Initialize the rails application
 Warehouse::Application.initialize!
