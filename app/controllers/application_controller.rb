c97ec8ccbf5765793473d4780e9b5b335df749ae
Mark Ronai <computadude@me.com>
2016-12-17 18:31:23 -0800

    rm old mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper :all
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :prepare_for_mobile
 
   helper_method :app_mode, :tradeshow?, :warehouse?, :order_name, :order_prefix
 
@@ -19,20 +18,6 @@ private
 
   def authenticate_admin!
     redirect_to new_user_session_path, alert: "Unathorized!", status: 403 and return false unless devise_controller? || current_user.is_admin?
-  end
-
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
   end
 
   def app_mode
